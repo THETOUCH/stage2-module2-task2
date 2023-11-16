e7b8740551975792420c9cc362de9d464ba30f95
THETOUCH <super.ilgiznex@gmail.com>
2023-11-16 18:54:19 +0600

    123

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,13 @@
 package com.example.listener;
 
-public class ContextListener {
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.time.LocalDateTime;
+
+public class ContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        event.getServletContext().setAttribute("servletTimeInit", LocalDateTime.now());
+    }
     //write your code here!
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,40 @@
 package com.example.servlet;
 
-public class LoginServlet {
+import com.example.Users;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebFilter("/login")
+public class LoginServlet extends HttpServlet {
     //write your code here!
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getSession().getAttribute("user") == null ) {
+            resp.sendRedirect("/login.jsp");
+        } else {
+            resp.sendRedirect("/user/hello.jsp");
+        }
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String requestLogin = req.getParameter("login");
+        String requestPassword = req.getParameter("password");
+        List<String> users = Users.getInstance().getUsers();
+        boolean isLoginRight = requestLogin != null && !requestLogin.isEmpty() && users.contains(requestLogin);
+        boolean isPasswordRight = requestPassword != null && !requestPassword.isEmpty() && !requestPassword.trim().isEmpty();
+        if (isLoginRight && isPasswordRight) {
+            req.getRequestDispatcher("/user/hello.jsp").forward(req, resp);
+            session.setAttribute("user", requestLogin);
+        } else {
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
 }
